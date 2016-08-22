4510c075586ace248d24855fd01cb1414f621baa
ImHimanShoe <goyalhimanshu414@gmail.com>
2016-08-22 21:08:06 +0530

    final score of 75/90 still improving was error in giving the output format

--- demo.R
+++ demo.R
@@ -7,15 +7,15 @@ write.table(mat, file.path( "H:/R workspace/test2.txt"), row.names = FALSE,col.
 write.table(mat, file.path("test2.txt"), sep = ",", row.names = FALSE,col.names = FALSE,
             qmethod = "double",append = FALSE)
 
-file_name = "F:/Assignment1/testcases/input/testcase1/input.txt";
+file_name = "F:/Assignment1/testcases/input/testcase2/input.txt";
 test <- read.table(file_name,header = FALSE,sep = ",")
 mat <- as.matrix(test)
-file_name1 = "F:/Assignment1/testcases/output/testcase1/out_1.txt";
+file_name1 = "F:/Assignment1/testcases/output/testcase2/out_1.txt";
 test1 <- read.table(file_name,header = FALSE,sep = ",")
 mat1 <- as.matrix(test)
-file_name2 = "F:/Assignment1/testcases/output/testcase1/out_2.txt";
+file_name2 = "F:/Assignment1/testcases/output/testcase2/out_2.txt";
 test1 <- read.table(file_name,header = FALSE,sep = ",")
 mat2 <- as.matrix(test)
-file_name3 = "F:/Assignment1/testcases/output/testcase1/out_3.txt";
+file_name3 = "F:/Assignment1/testcases/output/testcase2/out_3.txt";
 test3 <- read.table(file_name,header = FALSE,sep = ",")
 mat3 <- as.matrix(test)
--- submission_script.R
+++ submission_script.R
@@ -3,37 +3,111 @@
 #
 #"F:/7th semester/COL341 Rahul garg/input/input.txt"
 #test <- read.table("F:/7th semester/COL341 Rahul garg/input/input.txt",header = FALSE,sep = ",")
-file_name  = "F:/7th semester/COL341 Rahul garg/input/input.txt"
+file_name  = "F:/7th semester/COL341 Rahul garg/inputs/input.txt"
 args <- commandArgs(trailingOnly = TRUE)
 file_name <- args[1]
-file_name  = "F:/7th semester/COL341 Rahul garg/input/input.txt";
+file_name  = "F:/7th semester/COL341 Rahul garg/inputs/inputsmall1.txt";
 test <- read.table(file_name,header = FALSE,sep = ",")
 mat <- as.matrix(test)
 #mat =  matrix(c(5, 10,5, 8, 16, 7,9,18,2,4,8,7), nrow=3, ncol=4)
 dimns1 = dim(mat)
 y = mat[,dimns1[2]]  #for the ax=b equation
 
-mat = mat[,-dimns1[2]]
-mat = mat[-501,];
-y = y [-501];
+#mat = mat[,-dimns1[2]]
+#mat = mat[-501,];
+#y = y [-501];
 #y = t(y);
 #mat = t(mat);  #transpose to make coumn vector as row
 dimns = dim(mat)  
 lead = 1;
-thres = .0000001;
-rowCount = dimns[1]
-colCount = dimns[2]
+thres = .000001;
+rowCount = dimns[2]-1
+colCount = dimns[1];#remove -1 after it is due to y 
 i = 1 #edit this
 quit = FALSE
 row =1;
-m = mat
+m2 = mat;
+m2 = m2[,-dimns[2]]; #removing the y
+m2 = t(m2);  #this whie specific 
+#--------------------------------------------------------------------
 while(row <= rowCount && !quit)
-{
+{  #+1 because we have to process last one as well 
+  if(colCount+1<= lead)
+  {
+    quit = TRUE
+    break
+  }
   
-  #print(m);
-  #println();
+  i=row
   
-  #+1 because we have to process last one as well 
+  #finding the row with element at lead position is non zero to transfer it up.
+  while(! quit && (m2[i,lead] < thres) && (m2[i,lead] > -1*thres) )
+  {
+    # this is just for resetting the row when starting to search in next column for leading non-zero
+    #element
+    #this will increase the row number if zero
+    #below only if it is the last row than increase the column not row 
+    if(rowCount < i+1)
+    {
+      #resetting the row to start over this time for new lead
+      i=row;
+      lead = lead+1;
+      
+      if(colCount+1 <= lead)
+      {
+        quit = TRUE;
+        break;
+      }
+    }else {
+      i = i+1;
+    }
+  }
+  
+  if(!quit)
+  {
+    #swaping
+    temp = m2[i,];
+    tempy = y[i];
+    
+    m2[i,] = m2[row,]
+    m2[row,] = temp
+    
+    #swapRows(m, i, row);
+    
+    if(!((m2[row,lead] < thres) && (m2[row,lead] > -1*thres)) )
+    {
+      divider = m2[row,lead];
+      m2[row,] = m2[row,]/divider;
+    }  #  multiplyRow(m, row, 1.0f / m[row][lead]);
+    i = 1
+    while( i <= rowCount)
+    {
+      if(i != row)
+        
+      {  
+        subtract = m2[i,lead];
+        m2[i,] = m2[i,] - subtract*m2[row,]
+        }#subtractRows(m, m[i][lead], row, i);
+      i = i+1
+    }
+  }
+  row = row+1;
+}
+
+final3 = m2[rowSums(m2^2)>thres,] #only those row which are positive and sum is greater than some positive number
+final4 = t(final3);
+
+#<-------------------------------------------------------------->
+#resetting all the parameter
+lead = 1;
+rowCount = dimns[1]
+colCount = dimns[2]-1;#remove -1 after it is due to y 
+i = 1 #edit this
+quit = FALSE
+row =1;
+m = mat;
+while(row <= rowCount && !quit)
+{  #+1 because we have to process last one as well 
   if(colCount+1<= lead)
   {
     quit = TRUE
@@ -60,8 +134,7 @@ while(row <= rowCount && !quit)
         quit = TRUE;
         break;
       }
-    }
-    else {
+    }else {
       i = i+1;
     }
   }
@@ -101,33 +174,55 @@ while(row <= rowCount && !quit)
   }
   row = row+1;
 }
+#<-------------------------------------------------->
+
+y2 = m[,dimns1[2]]  #for the ax=b equation
 count =0;
-final = m[rowSums(m^2)>0,]
+m = m[,-dimns[2]]; #removing the y
+final = m[rowSums(m^2)>thres,] #only those row which are positive and sum is greater than some positive number
 lol = dim(final);
 lol[1] = lol[1]+1;
-null = final[,lol[1]:lol[2]]; #to ignore the null rows
-max = dim(null);
-y = t(y)
-demo2 = y;
-for(lv in 2:max[2]){
-  demo2 = rbind(demo2,y);
+if(lol[1]<lol[2]){
+  null = final[,lol[1]:lol[2]]; #to ignore the null rows
+  max = dim(null);
+  y = t(y)
+  demo2 = y;#for subtracting from the null matrix repeating y 
+  for(lv in 2:max[2]){
+    demo2 = rbind(demo2,y);
+  }
+  p = -1*max[1]-1;
+  q = -1*dim(demo2)[2]-1;
+  demo2 = demo2[,p:q]
+  
+  null = t(null)
+  
+  solly = null-demo2; #for third part
+  null = -1*null;  #for second part
+  
+  crntdim = dim(null);
+  temp = lol[2]-(lol[1]-1);#strength of the null space of a 
+  tempmat = diag(temp);
+  #just adding the total format....
+  solly2 = cbind(solly,tempmat);
+  null2 = cbind(null,tempmat)
+  null2 = t(null2)
+  final2 = t(final)
+}else{
+  null = 0;
+  null2 = null;
+  i = lol[2]+1;
+  j = length(y2);
+  tempry = y2[i:j];
+  if(j>=i && sum(tempry)>0){
+    solly2 = 0; #means no solution more constrain than variable
+  } else{
+    solly2 = y2[1:lol[2]]
+    
+  }
+  
+  
 }
-
-
-null = t(null)
-
-solly = null-demo2;
-null = -1*null;
-
-crntdim = dim(null);
-temp = lol[2]-lol[1]+1;#strength of the null space of a 
-tempmat = diag(temp);
-#just adding the total format....
-solly2 = cbind(solly,tempmat);
-null2 = cbind(null,tempmat)
-#null2 = t(null2)
-final2 = t(final)
-write.table(final, file.path("out_1.txt"), sep = ",", row.names = FALSE,col.names = FALSE,
+write.table(final4, file.path("out_1.txt"), sep = ",", row.names = FALSE,col.names = FALSE,
             qmethod = "double",append = FALSE);
 
 write.table(null2, file.path("out_2.txt"), sep = ",", row.names = FALSE,col.names = FALSE,
@@ -135,5 +230,4 @@ write.table(null2, file.path("out_2.txt"), sep = ",", row.names = FALSE,col.name
 
 write.table(solly2, file.path("out_3.txt"), sep = ",", row.names = FALSE,col.names = FALSE,
             qmethod = "double",append = FALSE);
-sink("myfile.txt", append=TRUE, split=TRUE)
 
