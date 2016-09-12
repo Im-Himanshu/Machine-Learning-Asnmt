974d56c9c4735fc4558596e36a53b372210f1691
ImHimanShoe <goyalhimanshu414@gmail.com>
2016-09-12 11:58:12 +0530

    creayed comparison in the test_logisitc

--- assnmt2/test_logistic.r
+++ assnmt2/test_logistic.r
@@ -19,7 +19,42 @@ theta <- as.matrix(result);
 result = A%*%t(theta);
 j = NROW(A);
 answer = c(1:j);
+netdata2 = rbind("Co-effic1","Co-effic2","Co-effic3","Co-effic4","Co-effic5","Co-effic6","Co-effic7",
+            "CCount1","CCount2","CCount3","CCount4","CCount5","CCount6","CCount7",
+             "ACount1","ACount2","ACount3","ACount4","ACount5","ACount6","ACount7",
+             "PCount1","PCount2","PCount3","PCount4","PCount5","PCount6","PCount7",
+             "accuracy1","accuracy2","accuracy3","accuracy4","accuracy5","accuracy6","accuracy7",
+             "netaccuracy%" , "Numberofcorrect"
+             )
+
+
+
+
+
+
+
+
+
+netdata = netdata2;
+
+coeff = t(matrix(c(1.6, 1.2, 1.6, 1 , 1 , 1.6, 1,
+                   1.8, 1.2, 1  , 1 , 1 , 1  , 1,
+                   1.2, 1.8, 1  , 1 , 1 , 1  ,1,
+                   1.5, 1.2, 1.6, 1 , 1 , 1  ,1,
+                   1.5, 1.2, 1.6, 1 , 1 , 1.7,1
+                   ),7,5))
+for(x in 1:NROW(coeff)){
+
 i = 0;
+result[,1] = result[,1]*coeff[x,1];
+result[,2] = result[,2]*coeff[x,2];
+result[,3] = result[,3]*coeff[x,3];
+result[,4] = result[,4]*coeff[x,4];
+result[,5] = result[,5]*coeff[x,5]
+result[,6] = result[,6]*coeff[x,6]
+result[,7] = result[,7]*coeff[x,7];
+
+
 for (i in 1:j){
   pmax = which.max( result[i,] )
   answer[i] = pmax
@@ -29,3 +64,38 @@ for (i in 1:j){
 
 write.table(answer, file.path(model_result), sep = ",", row.names = FALSE,col.names = FALSE,
             qmethod = "double",append = FALSE);
+
+##########checking accuracy 
+real  = "H:/R workspace/assnmt2/files1/public_solution2.txt";
+input <- read.table(real,header = FALSE,sep = ",")
+rAnswer <- t(t(as.matrix(input)));
+u = NROW(rAnswer);
+#answer[] = 1;
+Ccount = 1:NROW(theta);
+Ccount[] = 0#correct count
+Acount = t(Ccount)
+Ccount = Acount
+Pcount = Acount
+accuracy = Pcount
+for(q in 1:u){
+  Acount[rAnswer[q]] = Acount[rAnswer[q]]+1
+  Pcount[answer[q]] = Pcount[answer[q]]+1
+  
+  if(rAnswer[q] == answer[q]){
+    Ccount[answer[q]] = Ccount[answer[q]]+1
+  }
+}
+for(z in 1:NROW(theta)){
+  accuracy[z] = Ccount[z]*100/Acount[z];
+}
+
+#percentage = t(t(percentage))
+accurate = sum(Ccount)
+total = accurate*100/150000
+
+data = matrix(1:1,1,1)
+coefficent = t(coeff[x,])
+data = rbind(t(coefficent),t(Ccount),t(Acount),t(Pcount),t(accuracy),total,accurate)
+netdata = cbind(netdata,data)
+}
+
--- assnmt2/train_logistic.r
+++ assnmt2/train_logistic.r
@@ -4,16 +4,12 @@
 # Train code for logistic regression part goes here
 
 args <- commandArgs(trailingOnly = TRUE)
-example = 300; ##no of test data per example it should be equal for all
-##assuming all of the cllasses have minimum of this much cases 
 
 file_name <- args[1]
 modeltheta <- args[2]
 file_name  = "H:/R workspace/assnmt2/files1/input2.csv";
 modeltheta = "sol2.txt";
 input <- read.table(file_name,header = FALSE,sep = ",")
-dimn = dim(input);
-input = input[order(input$V55),]
 mat <- as.matrix(input);
 dimn = dim(mat)
 m = dimn[1]
@@ -21,14 +17,25 @@ n = dimn[2]-1
 y = mat[,n+1]
 mat = mat[,-(n+1)]
 x = t(t(mat));
+
 ##################create more feature here if want 
 #for(j in 1:n){
- #}
+#}
 #dimn = dim(x)
 #m = dimn[1]
 #n = dimn[2]
 #-----------------------------------------------------#
-max = y[m];
+
+#calculating number of classes
+max = 0;
+for(i in y){
+  if(i>max){
+    max =i;
+  }
+}
+
+
+#calculating til for all example 
 til = matrix(1:max*m,m,max)
 til[,] = 0;
 h = NROW(y)
@@ -36,63 +43,43 @@ for(p in 1:h){
   yi = y[p]
   til[p,yi] = 1;
 }
-#counting the total number of each class
-counti = 1:max;
-mid = 0;
-for(w in 1:(max-1)){
-  counti[w+1] = mid+sum(til[,w]);
-  mid = counti[w+1];
-}
-
-
 #initializing theta
 classes = max;
 feature = n
 #count occurnce 
+count = sum(til[,1])
+
+
+
 theta = matrix(1:max*n,classes,feature)
 ##########################7 rows x   54 column
 theta[,] = 0;
 theta2 = theta;
 example = 1;
 alpha = .0000001;
-
-
-#cases = t(t(c(1:example,counti[1]:(counti[1]+example),counti[2]:(counti[2]+example),
- #         counti[3]:(counti[3]+example),counti[4]:(counti[4]+example),
-  #        counti[5]:(counti[5]+example))));
-for(data in 1:example)
-{##first example of all second example of all one by one not like
-  ## all ones together and all twos together
-  for(ds in 1:(classes-2)){
-    example = (counti[ds]+data)
-    
-    denom = 0
-    num= 0;
-    seeme = 1:classes;
-    for(c in 1:classes){
-      seeme[c]  =t(theta[c,])%*%x[example,]
-      denom = denom + 2.72^(seeme[c])
-    }
-    for(a in 1:classes){
-      seeme2 = t(theta[a,])%*%x[example,];
-      num = 2.72^(t(theta[a,])%*%x[example,])
-      prob = num/denom
-      for(b in 1:feature){
-        #b=b+1;
-        gradient = x[example,b]*(til[example,a]-prob)
-        theta2[a,b] = theta[a,b] + alpha*gradient;
-      }
-    }
-    theta3 = theta
-    theta = theta2;
-    example = example+1
-    
-    
-    
+for(example in 1:300000)
+{
+  denom = 0
+  num= 0;
+  seeme = 1:classes;
+  for(c in 1:classes){
+    seeme[c]  =t(theta[c,])%*%x[example,]
+    denom = denom + 2.72^(seeme[c])
   }
-  
+  for(a in 1:classes){
+    seeme2 = t(theta[a,])%*%x[example,]; #can also be taken from the seeme[a]
+    num = 2.72^(t(theta[a,])%*%x[example,])
+    prob = num/denom
+    for(b in 1:feature){
+      #b=b+1;
+      gradient = x[example,b]*(til[example,a]-prob)
+      theta2[a,b] = theta[a,b] + alpha*gradient;
+    }
+  }
+  #theta3 = theta 
+  theta = theta2;
+  #example = example+1
 }
 
 write.table(theta, file.path(modeltheta), sep = ",", row.names = FALSE,col.names = FALSE,
             qmethod = "double",append = FALSE);
-
